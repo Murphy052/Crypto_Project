67dd1045770327706a9337f531f19b3fdb4fbd6a
Murphy052 <iguliyev@proton.me>
2024-05-09 17:06:59 +0400

    Create client frontend

---
+++ src/client/render.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
+
+from src.db.repositories import user_repository
+
+templates = Jinja2Templates(directory="src/client/templates")
+
+router = APIRouter()
+
+
+@router.get("/board")
+async def render_board(request: Request):
+    texts_data = user_repository.get_all()
+
+    print(texts_data)
+    return templates.TemplateResponse("board.html", {"request": request})
+
+
+@router.get("/login")
+async def render_login(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
--- src/main.py
+++ src/main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI, Request
 from contextlib import asynccontextmanager
 
 from starlette.middleware.authentication import AuthenticationMiddleware
+from starlette.staticfiles import StaticFiles
 
 from src.api import routes
+from src.client.render import router as render
 from src.db import db
 from src.core.middleware import AuthBackend
 from src.db.repositories import user_repository
@@ -19,6 +21,8 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(AuthenticationMiddleware, backend=AuthBackend())
 
+app.mount("/static", StaticFiles(directory="src/client/static"), name="static")
+
 
 @app.get("/")
 def get_main(request: Request):
@@ -31,3 +35,4 @@ async def get_user():
 
 
 app.include_router(routes)
+app.include_router(render)
